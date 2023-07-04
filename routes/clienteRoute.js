d217d92422c9354abccb48bb8ba9dd3a526ea6f9
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2023-07-04 14:55:40 -0300

    rotas do cliente

--- routes/clienteRoute.js
+++ routes/clienteRoute.js
@@ -5,5 +5,7 @@ const controller = require('../controller/clienteController')
 routes.get('/', controller.principal)
 routes.get('/produto/:id', controller.detalhe)
 routes.get('/logar', controller.abrecadastro)
+routes.post('/logar', controller.abrecadastro)
+routes.get('/logar', controller.abrecadastro)
 
 module.exports = routes
